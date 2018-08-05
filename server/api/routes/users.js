49c4788f4c74c30616c1067876bca75e97df8cf2
mahmoud-hany <mahmoud.hany.5220@gmail.com>
2018-08-05 18:25:32 +0200

    Adding User Login & JWT Signing

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
-// create new user
+// create new user [ User sing UP]
 router.post('/signup', (req, res) => {
     // search for the provided email 
     User.find({email: req.body.email}).then(user => {
@@ -41,6 +43,52 @@ router.post('/signup', (req, res) => {
         res.status(400).json(error);
     })
 }); 
+
+
+// User sign in
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const pass = req.body.password;
+
+    User.findOne({email}).then( user => {
+        if ( !user ) {
+            return res.status(401).json({ // 401 means un authorized
+                message: 'Auth failed',
+            });
+        }
+        
+        bcrypt.compare(pass, user.password, (err, result) => { // password - hashed password
+            if (err) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            
+            if (result) { // if it true means that password of the user is match hashed one
+                const data = {
+                    email,
+                    _id: user._id
+                };
+
+                const token = jwt.sign(data, 'MAHMOUD', { expiresIn: "1h"});
+
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token
+                })
+            }
+
+            res.status(409).json({
+                message: 'Auth failed',
+                err
+            });
+        });
+        
+    }).catch( err => {
+        res.status(400).json(err);
+
+    });
+})
 
 //delete user
 router.delete('/:userId', (req, res) => {
